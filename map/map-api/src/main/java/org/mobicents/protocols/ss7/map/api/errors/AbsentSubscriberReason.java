cf31f9b636454a1deaa4c64c68b2062acd1ca4ad
SummaNetworks <info@onevox.io>
2016-08-16 18:17:02 +0200

    Added mtRoamingRetry (4), busySubscriber (5) reasons to AbsentSubscriberReason

--- map/map-api/src/main/java/org/mobicents/protocols/ss7/map/api/errors/AbsentSubscriberReason.java
+++ map/map-api/src/main/java/org/mobicents/protocols/ss7/map/api/errors/AbsentSubscriberReason.java
@@ -23,19 +23,33 @@
 package org.mobicents.protocols.ss7.map.api.errors;
 
 /**
+ *<code>
+ *      AbsentSubscriberReason ::= ENUMERATED {
+ *      imsiDetach (0),
+ *      restrictedArea (1),
+ *      noPageResponse (2),
+ *      ... ,
+ *      purgedMS (3),
+ *      mtRoamingRetry (4),
+ *      busySubscriber (5)}
  *
- * AbsentSubscriberReason ::= ENUMERATED { imsiDetach (0), restrictedArea (1), noPageResponse (2), ... , purgedMS (3)} --
- * exception handling: at reception of other values than the ones listed the -- AbsentSubscriberReason shall be ignored. -- The
- * AbsentSubscriberReason: purgedMS is defined for the Super-Charger feature -- (see TS 23.116). If this value is received in a
- * Provide Roaming Number response -- it shall be mapped to the AbsentSubscriberReason: imsiDetach in the Send Routeing --
- * Information response
- *
+ * -- exception handling: at reception of other values than the ones listed the
+ * -- AbsentSubscriberReason shall be ignored.
+ * -- The AbsentSubscriberReason: purgedMS is defined for the Super-Charger feature
+ * -- (see TS 23.116). If this value is received in a Provide Roaming Number response
+ * -- it shall be mapped to the AbsentSubscriberReason: imsiDetach in the Send Routeing
+ * -- Information response
+ * -- The AbsentSubscriberReason: mtRoamingRetry is used during MT Roaming Retry,
+ * -- see 3GPP TS 23.018[97].
+ * -- The AbsentSubscriberReason: busySubscriber is used during MT Roaming Forwarding,
+ * -- see 3GPP TS 23.018[97].
+ * </code>
  *
  * @author sergey vetyutnev
  *
  */
 public enum AbsentSubscriberReason {
-    imsiDetach(0), restrictedArea(1), noPageResponse(2), purgedMS(3);
+    imsiDetach(0), restrictedArea(1), noPageResponse(2), purgedMS(3), mtRoamingRetry (4), busySubscriber (5);
 
     private int code;
 
@@ -57,6 +71,10 @@ public enum AbsentSubscriberReason {
                 return noPageResponse;
             case 3:
                 return purgedMS;
+            case 4:
+                return mtRoamingRetry;
+            case 5:
+                return busySubscriber;
             default:
                 return null;
         }
