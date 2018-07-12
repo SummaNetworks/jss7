f3d92e1a20cc7b817adf952d3edba19bd45c79b3
SummaNetworks <info@onevox.io>
2018-07-12 12:36:28 +0200

    TXC-38 - CallBarringData is not correctly encoded

--- map/map-impl/src/main/java/org/mobicents/protocols/ss7/map/service/mobility/subscriberInformation/CallBarringDataImpl.java
+++ map/map-impl/src/main/java/org/mobicents/protocols/ss7/map/service/mobility/subscriberInformation/CallBarringDataImpl.java
@@ -9,8 +9,8 @@ import org.mobicents.protocols.asn.AsnOutputStream;
 import org.mobicents.protocols.asn.Tag;
 import org.mobicents.protocols.ss7.map.api.MAPException;
 import org.mobicents.protocols.ss7.map.api.MAPParsingComponentException;
+import org.mobicents.protocols.ss7.map.api.MAPParsingComponentExceptionReason;
 import org.mobicents.protocols.ss7.map.api.primitives.MAPExtensionContainer;
-import org.mobicents.protocols.ss7.map.api.service.mobility.subscriberInformation.AbstractMAPAsnPrimitive;
 import org.mobicents.protocols.ss7.map.api.service.mobility.subscriberInformation.CallBarringData;
 import org.mobicents.protocols.ss7.map.api.service.mobility.subscriberManagement.ExtCallBarringFeature;
 import org.mobicents.protocols.ss7.map.api.service.supplementary.Password;
@@ -19,6 +19,7 @@ import org.mobicents.protocols.ss7.map.datacoding.NullEncoderFacility;
 import org.mobicents.protocols.ss7.map.datacoding.ObjectEncoderFacility;
 import org.mobicents.protocols.ss7.map.primitives.MAPAsnPrimitive;
 import org.mobicents.protocols.ss7.map.primitives.MAPExtensionContainerImpl;
+import org.mobicents.protocols.ss7.map.primitives.SequenceBase;
 import org.mobicents.protocols.ss7.map.service.mobility.subscriberManagement.ExtCallBarringFeatureImpl;
 import org.mobicents.protocols.ss7.map.service.supplementary.PasswordImpl;
 
@@ -41,7 +42,7 @@ import org.mobicents.protocols.ss7.map.service.supplementary.PasswordImpl;
  *
  * @author eva ogallar
  */
-public class CallBarringDataImpl extends AbstractMAPAsnPrimitive implements CallBarringData, MAPAsnPrimitive {
+public class CallBarringDataImpl extends SequenceBase implements CallBarringData, MAPAsnPrimitive {
 
     public static final String _PrimitiveName = "CallBarringData";
 
@@ -52,10 +53,12 @@ public class CallBarringDataImpl extends AbstractMAPAsnPrimitive implements Call
     private MAPExtensionContainer extensionContainer;
 
     public CallBarringDataImpl() {
+        super(_PrimitiveName);
     }
 
     public CallBarringDataImpl(ArrayList<ExtCallBarringFeature> callBarringFeatureList, Password password,
                                Integer wrongPasswordAttemptsCounter, boolean notificationToCSE, MAPExtensionContainer extensionContainer) {
+        super(_PrimitiveName);
         this.callBarringFeatureList = callBarringFeatureList;
         this.password = password;
         this.wrongPasswordAttemptsCounter = wrongPasswordAttemptsCounter;
@@ -102,16 +105,16 @@ public class CallBarringDataImpl extends AbstractMAPAsnPrimitive implements Call
     public boolean getIsPrimitive() {
         return false;
     }
-
     protected void _decode(AsnInputStream ansIS, int length) throws AsnException, IOException, MAPParsingComponentException {
 
         password = null;
         wrongPasswordAttemptsCounter = null;
-        callBarringFeatureList = new ArrayList<ExtCallBarringFeature>();
+        callBarringFeatureList = null;
         notificationToCSE = false;
         extensionContainer = null;
 
         AsnInputStream ais = ansIS.readSequenceStreamData(length);
+
         int num = 0;
         while (true) {
             if (ais.available() == 0)
@@ -123,9 +126,30 @@ public class CallBarringDataImpl extends AbstractMAPAsnPrimitive implements Call
                 switch (tag) {
                     case Tag.SEQUENCE:
                         if (num==0) {
-                            this.callBarringFeatureList.add((ExtCallBarringFeature) ObjectEncoderFacility.
-                                    decodeObject(ais, new ExtCallBarringFeatureImpl(), "callBarringFeatureList", getPrimitiveName()));
-                        }else {
+
+                            // decode callBarringFeatureList
+                            AsnInputStream ais2 = ais.readSequenceStream();
+                            while (true) {
+                                if (ais2.available() == 0)
+                                    break;
+
+                                tag = ais2.readTag();
+                                if (tag != Tag.SEQUENCE || ais2.getTagClass() != Tag.CLASS_UNIVERSAL || ais2.isTagPrimitive())
+                                    throw new MAPParsingComponentException("Error while decoding " + _PrimitiveName
+                                            + ".extCallBarringFeature: Parameter extCallBarringFeature is primitive",
+                                            MAPParsingComponentExceptionReason.MistypedParameter);
+
+                                this.callBarringFeatureList.add((ExtCallBarringFeature) ObjectEncoderFacility.
+                                        decodeObject(ais, new ExtCallBarringFeatureImpl(), "callBarringFeatureList", getPrimitiveName()));
+
+                            }
+
+                            if (this.callBarringFeatureList.size() < 1 || this.callBarringFeatureList.size() > 32) {
+                                throw new MAPParsingComponentException("Error while decoding " + _PrimitiveName
+                                        + ": Parameter callBarringFeatureList size must be from 1 to 32, found: "
+                                        + this.callBarringFeatureList.size(), MAPParsingComponentExceptionReason.MistypedParameter);
+                            }
+                        } else {
                             extensionContainer = (MAPExtensionContainer) ObjectEncoderFacility.
                                     decodeObject(ais, new MAPExtensionContainerImpl(), "extensionContainer", getPrimitiveName());
                         }
@@ -149,36 +173,83 @@ public class CallBarringDataImpl extends AbstractMAPAsnPrimitive implements Call
         }
    }
 
+    private String getPrimitiveName() {
+        return _PrimitiveName;
+    }
+
     public void encodeData(AsnOutputStream asnOs) throws MAPException {
 
         if (this.callBarringFeatureList == null || this.callBarringFeatureList.size() < 1
                 || this.callBarringFeatureList.size() > 32) {
             throw new MAPException("CallBarringFeatureList list must contains from 1 to 32 elements");
         }
+        try {
+            asnOs.writeTag(Tag.CLASS_UNIVERSAL, false, Tag.SEQUENCE);
+            int pos = asnOs.StartContentDefiniteLength();
+            for (ExtCallBarringFeature extCallBarringFeature : this.callBarringFeatureList) {
+                ((ExtCallBarringFeatureImpl) extCallBarringFeature).encodeAll(asnOs);
+            }
+            asnOs.FinalizeContent(pos);
 
-        for (ExtCallBarringFeature extCallBarringFeature : this.callBarringFeatureList) {
-            ((ExtCallBarringFeatureImpl) extCallBarringFeature).encodeAll(asnOs);
+            if (this.password != null) {
+                ((PasswordImpl) this.password).encodeAll(asnOs);
+            }
+
+            if (this.wrongPasswordAttemptsCounter != null) {
+                if (this.wrongPasswordAttemptsCounter < 0 || this.wrongPasswordAttemptsCounter > 4) {
+                    throw new MAPException("Error while encoding " + _PrimitiveName
+                            + " parameter wrongPasswordAttemptsCounter is out of range (0..4). Actual value: " + this.wrongPasswordAttemptsCounter);
+                }
+
+                IntegerEncoderFacility.encode(asnOs, wrongPasswordAttemptsCounter, "wrongPasswordAttemptsCounter");
+            }
+
+            NullEncoderFacility.encode(asnOs, this.notificationToCSE, "notificationToCSE");
+
+            if (this.extensionContainer != null) {
+                ((MAPExtensionContainerImpl) this.extensionContainer).encodeAll(asnOs);
+            }
+        } catch (AsnException e) {
+            throw new MAPException("AsnException when encoding " + _PrimitiveName + ": " + e.getMessage(), e);
         }
-
-        if (this.password != null) {
-            ((PasswordImpl) this.password).encodeAll(asnOs);
-        }
-
-        if (this.wrongPasswordAttemptsCounter != null) {
-            IntegerEncoderFacility.encode(asnOs, wrongPasswordAttemptsCounter, "wrongPasswordAttemptsCounter");
-        }
-
-        NullEncoderFacility.encode(asnOs, this.notificationToCSE, "notificationToCSE");
-
-        if (this.extensionContainer != null) {
-            ((MAPExtensionContainerImpl) this.extensionContainer).encodeAll(asnOs);
-        }
-
     }
 
     @Override
-    protected String getPrimitiveName() {
-        return _PrimitiveName;
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(_PrimitiveName);
+        sb.append(" [");
+
+        if (this.callBarringFeatureList != null) {
+            sb.append("callBarringFeatureList=[");
+            boolean firstItem = true;
+            for (ExtCallBarringFeature extCallBarringFeature: callBarringFeatureList) {
+                if (firstItem) {
+                    firstItem = false;
+                } else {
+                    sb.append(", ");
+                }
+                sb.append(extCallBarringFeature);
+            }
+            sb.append("]");
+        }
+        if (this.password != null) {
+            sb.append(", password=");
+            sb.append(this.password);
+        }
+        if (this.wrongPasswordAttemptsCounter != null) {
+            sb.append(", wrongPasswordAttemptsCounter=");
+            sb.append(this.wrongPasswordAttemptsCounter);
+        }
+        if (this.notificationToCSE) {
+            sb.append(", notificationToCSE");
+        }
+        if (this.extensionContainer != null) {
+            sb.append(", extensionContainer=");
+            sb.append(this.extensionContainer);
     }
 
+        sb.append("]");
+        return sb.toString();
+    }
 }
