386e0e856660a4ae70277801d02c2ccf47039bd4
ajimenez <antonio.jimenez@summanetworks.com>
2022-06-16 13:22:36 +0200

    [TXC-157] Update the heartbeat behaviour in topic.

--- topic/src/main/java/com/summanetworks/topic/TopicSccpMessage.java
+++ topic/src/main/java/com/summanetworks/topic/TopicSccpMessage.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.log4j.Logger;
